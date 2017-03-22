f293fdc33dbfe035338ec24a9ccfff65c6102d53
soukwinder <sanjosinghmaddan@hotmail.com>
2017-03-22 13:39:15 +0100

    json parsed

--- app.js
+++ app.js
@@ -3,7 +3,14 @@
  */
 
 var http = require('client-http');
+var moment = require('moment')
 
-http.get("http://localhost:3001/api/temp", function(data){
-    data && console.log(data);
-});
+var timer = setInterval(getData, 1000);
+
+function getData () {
+    http.get("http://localhost:3001/api/temp", function(data){
+        var json = JSON.parse(data)
+        console.log(json.DateTime);
+
+    });
+}
