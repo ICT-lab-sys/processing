6ecd3744e2ad56f22a490125d560a8751f365eed
soukwinder <sanjosinghmaddan@hotmail.com>
2017-03-24 11:36:22 +0100

    process done + export db

--- app.js
+++ app.js
@@ -4,13 +4,26 @@
 
 var http = require('client-http');
 var moment = require('moment')
+var MongoClient = require('mongodb').MongoClient
 
 var timer = setInterval(getData, 1000);
 
 function getData () {
     http.get("http://localhost:3001/api/temp", function(data){
         var json = JSON.parse(data)
-        console.log(json.DateTime);
+        var day = new Date(json.DateTime).getDate()
+        var month = new Date(json.DateTime).getMonth()+1
+        var year = new Date(json.DateTime).getFullYear()
+        var hours = new Date(json.DateTime).getHours()
+        var processedData = JSON.parse('{"Jaar":'+year+', "Maand":'+month+', "Dag":'+day+', "Uur":'+hours+', "Temperatuur":'+json.Temperature+'}')
+
+        console.log(processedData);
+
+        MongoClient.connect('mongodb://localhost:27017/ictlab', function (err, db) {
+            if (err) throw err
+
+            db.collection('temp').insertOne(processedData)
+        })
 
     });
 }
