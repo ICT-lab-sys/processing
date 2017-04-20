4d06b87a84660db035aa7be407624a9125e8a293
soukwinder <sanjosinghmaddan@hotmail.com>
2017-04-20 08:52:34 +0200

    humidity

--- app.js
+++ app.js
@@ -4,15 +4,17 @@
 
 var http = require('client-http');
 var moment = require('moment')
-var MongoClient = require('mongodb').MongoClient
-var oldData = "dfsd"
+var MongoClient = require('mongodb').MongoClient;
+var oldDataTemp = "dfsd"
+var oldDataHumidity = "dfsd"
 
-var timer = setInterval(getData, 1000);
+var timerTemp = setInterval(getDataTemp, 1000);
+var timerHumidity = setInterval(getDataHumidity, 1000);
 
-function getData () {
+function getDataTemp () {
     http.get("http://localhost:3001/api/streamdata/temp", function(data){
-        var json = JSON.parse(data)
-        var day = new Date(json.DateTime).getDate()
+        var json = JSON.parse(data);
+        var day = new Date(json.DateTime).getDate();
         var month = new Date(json.DateTime).getMonth()+1
         var year = new Date(json.DateTime).getFullYear()
         var hours = new Date(json.DateTime).getHours()
@@ -20,14 +22,37 @@ function getData () {
 
        // console.log(processedData);
 
-        if(JSON.stringify(oldData) != JSON.stringify(json)) {
-           // var test = oldData
-            oldData = json;
-            console.log(JSON.stringify(oldData) + "  ," + JSON.stringify(json))
+        if(JSON.stringify(oldDataTemp) != JSON.stringify(json)) {
+            oldDataTemp = json;
+            console.log(JSON.stringify(oldDataTemp) + "  ," + JSON.stringify(json))
             MongoClient.connect('mongodb://localhost:27017/ictlab', function (err, db) {
                 if (err) throw err
 
                 db.collection('temp').insertOne(processedData)
+            })
+        }
+    });
+}
+
+
+function getDataHumidity () {
+    http.get("http://localhost:3001/api/streamdata/humidity", function(data){
+        var json = JSON.parse(data);
+        var day = new Date(json.DateTime).getDate();
+        var month = new Date(json.DateTime).getMonth()+1
+        var year = new Date(json.DateTime).getFullYear()
+        var hours = new Date(json.DateTime).getHours()
+        var processedData = JSON.parse('{"Dag":'+day+', "Maand":'+month+', "Jaar":'+year+', "Uur":'+hours+', "Luchtvochtigheid":'+json.Humidity+'}')
+
+        // console.log(processedData);
+
+        if(JSON.stringify(oldDataHumidity) != JSON.stringify(json)) {
+            oldDataHumidity = json;
+            console.log(JSON.stringify(oldDataHumidity) + "  ," + JSON.stringify(json))
+            MongoClient.connect('mongodb://localhost:27017/ictlab', function (err, db) {
+                if (err) throw err
+
+                db.collection('humidity').insertOne(processedData)
             })
         }
     });
